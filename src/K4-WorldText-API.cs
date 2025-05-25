2c68c65d75a00223b3105dc2963a2e159fd04108
M-archand <13469995+M-archand@users.noreply.github.com>
2025-05-24 23:23:43 -0400

    Update plugin version (with changes by DEADLYDEVIL-IR)

--- src/K4-WorldText-API.cs
+++ src/K4-WorldText-API.cs
@@ -20,7 +20,7 @@ public class Plugin : BasePlugin
     public List<WorldTextConfig> loadedConfigs = new();
     public List<MultilineWorldText> multilineWorldTexts = new();
     public override string ModuleName => "CS2 WorldText API";
-    public override string ModuleVersion => "1.2.3";
+    public override string ModuleVersion => "1.2.4";
     public override string ModuleAuthor => "K4ryuu";
 
     public static PluginCapability<IK4WorldTextSharedAPI> Capability_SharedAPI { get; } = new("k4-worldtext:sharedapi");
